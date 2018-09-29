6d826d45a8d0c6657496a081447424911329ee30
Gonlos <gonzalo.cortazar.kao@gmail.com>
2018-09-29 17:02:28 +0200

    config challenge

--- router/slack.js
+++ router/slack.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-router.get('/',(req,res,next)=>{
-  res.json({"text": "Hello, world!"})
-})
+router.get("/", (req, res, next) => {
+  res.json({ text: "Hello, world!" });
+});
+router.post("/auth", (req, res, next) => {
+  const {challenge}=req.body
+  console.log(req.body)
+  res.json({challenge});
+});
 
 module.exports = router;
