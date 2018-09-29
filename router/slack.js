0d1a2cfc4eaeb3dbba28db61779e78c4861ae53b
Gonlos <gonzalo.cortazar.kao@gmail.com>
2018-09-29 17:43:18 +0200

    slash commands

--- router/slack.js
+++ router/slack.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
+  console.log(req)
   res.json({ text: "Hello, world!" });
 });
 router.post("/auth", (req, res, next) => {
@@ -9,5 +10,13 @@ router.post("/auth", (req, res, next) => {
   console.log(req.body)
   res.json({challenge});
 });
+router.post("/", (req, res, next) => {
+  const {challenge}=req.body
+  console.log(req.body)
+  res.json({text:"algo recibo"});
+});
+router.post("/start",(req,res,next)=>{
+  res.json({text:"Esto es para empezar"})
+})
 
 module.exports = router;
