7d91954ddf6afa1eef775736df153f1d3897b03e
Gonlos <gonzalo.cortazar.kao@gmail.com>
2018-10-07 00:55:28 +0200

    refactor wakeUp

--- Gordify.js
+++ Gordify.js
@@ -1,55 +1,50 @@
 const SlackBot = require('./models/SlackBot')
 
 class Gordify {
-  constructor(){
-    this.name = "Gordify"
+  constructor(name){
+    this.name = name || "Gordify"
     this.channel = "general"
     this.days = "thursday"
     this.time = "10:00"
     this.duration = "2"
     this.jobs = []
-    SlackBot.find()
-    .then(bots=>{
-      if(bots.length==0){
-        this.createBot()
-      }else{
-        console.log(bots[0])
-        this.WakeUpBot(bots[0])
-      }
-    })
-    .catch(e=>{
-      console.log("Error find",e)
-    })
+    this.WakeUpBot()
   }
 
   createBot(){
-    SlackBot.create({
+    return SlackBot.create({
       name:this.name,
       channel:this.channel,
       days:this.days,
       time:this.time,
       duration:this.duration
     })
+  }
+
+  WakeUpBot(){
+    SlackBot.findOne({name:this.name})
     .then(bot=>{
-      console.log(bot)
-      console.log(this)
+      if(!bot){
+        return this.createBot()
+      }else{
+        this.name = bot.name
+        this.channel = bot.channel
+        this.days = bot.days
+        this.time = bot.time
+        this.duration = bot.duration
+        return bot
+      }
+    })
+    .then(bot=>console.log("WakeUp",bot))
+    .catch(e=>{
+      console.log("Error wake",e)
     })
   }
 
-  WakeUpBot(bot){
-    this.name = bot.name
-    this.channel = bot.channel
-    this.days = bot.days
-    this.time = bot.time
-    this.duration = bot.duration
-    console.log("whakeUp",bot)
-    console.log("whakeUp",this)
-  }
-
   start(){}
-  
+
   stop(){}
-  
+
   makeGroups(users) {
     users.sort(() => Math.random() - 0.5)
     nUsers = users.length
